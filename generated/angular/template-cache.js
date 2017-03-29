511d9e235cd7f2a85438d48f6a40c42b1f8e056b
stéphane coulomb <prakamsay28@gmail.com>
2017-03-29 17:10:05 +0200

    first commit

---
+++ generated/angular/template-cache.js
@@ -0,0 +1,170 @@
+angular.module("app").run(["$templateCache", function($templateCache) {
+
+  $templateCache.put("anon/home.html",
+    "<h1>Hello World</h1>"
+  );
+
+  $templateCache.put("anon/login.html",
+    "<div class=\"row\">\n" +
+    "    <div class=\"col-xs-6 col-xs-offset-3\">\n" +
+    "        <form class=\"form\" name=\"loginForm\" novalidate ng-submit=\"login()\">\n" +
+    "            <div ng-repeat=\"error in errors\">{{error.error}}</div>\n" +
+    "            <div class=\"input-group\">\n" +
+    "                <span class=\"input-group-addon\"><i class=\"glyphicon glyphicon-user\"></i></span>\n" +
+    "                <input id=\"email\" type=\"email\" class=\"form-control\" ng-model=\"user.email\" required placeholder=\"Email Address\">\n" +
+    "            </div>\n" +
+    "            <div class=\"input-group\">\n" +
+    "                <span class=\"input-group-addon\"><i class=\"glyphicon glyphicon-lock\"></i></span>\n" +
+    "                <input id=\"password\" type=\"password\" class=\"form-control\" ng-model=\"user.password\" required placeholder=\"Password\">\n" +
+    "            </div>\n" +
+    "            <button type=\"submit\" class=\"btn btn-primary btn-block\">Login</button>\n" +
+    "        </form>\n" +
+    "    </div>\n" +
+    "</div>\n"
+  );
+
+  $templateCache.put("anon/navbar.html",
+    "<nav class=\"navbar navbar-default\" role=\"navigation\" ng-controller=\"NavbarController\">\n" +
+    "    <div class=\"container-fluid\">\n" +
+    "        <div class=\"navbar-header\">\n" +
+    "            <button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\"#navbar\">\n" +
+    "        <span class=\"sr-only\">Toggle navigation</span>\n" +
+    "        <span class=\"icon-bar\"></span>\n" +
+    "        <span class=\"icon-bar\"></span>\n" +
+    "        <span class=\"icon-bar\"></span>\n" +
+    "      </button>\n" +
+    "            <a class=\"navbar-brand\" href=\"#\"></a>\n" +
+    "        </div>\n" +
+    "        <div class=\"collapse navbar-collapse\" id=\"navbar\">\n" +
+    "            <ul class=\"nav navbar-nav\">\n" +
+    "                <li ui-sref-active=\"active\"><a ui-sref=\"anon.home\">Home</a></li>\n" +
+    "\n" +
+    "            </ul>\n" +
+    "            <ul class=\"nav navbar-nav navbar-right\">\n" +
+    "                <li>\n" +
+    "                    <li ui-sref-active=\"active\"><a ui-sref=\"anon.login\" ng-hide=\"auth.isAuthenticated()\">Login</a></li>\n" +
+    "                    <li ui-sref-active=\"active\"><a ui-sref=\"anon.register\" ng-hide=\"auth.isAuthenticated()\">Register</a></li>\n" +
+    "                    <li ui-sref-active=\"active\"><a ui-sref=\"user.dashboard\" ng-show=\"auth.isAuthenticated()\">Dashboard</a></li>\n" +
+    "                    <li><a ng-click=\"logout()\" ng-show=\"auth.isAuthenticated()\" href='#'>Logout</a></li>\n" +
+    "                </li>\n" +
+    "            </ul>\n" +
+    "        </div>\n" +
+    "    </div>\n" +
+    "</nav>\n"
+  );
+
+  $templateCache.put("anon/register.html",
+    "<div class=\"row\">\n" +
+    "    <div class=\"col-xs-6 col-xs-offset-3\">\n" +
+    "        <form class=\"form\" name=\"loginForm\" novalidate ng-submit=\"register()\">\n" +
+    "            <div ng-repeat=\"error in errors\">{{error.error}}</div>\n" +
+    "            <div class=\"input-group\">\n" +
+    "                <span class=\"input-group-addon\"><i class=\"glyphicon glyphicon-user\"></i></span>\n" +
+    "                <input id=\"email\" type=\"email\" class=\"form-control\" ng-model=\"user.email\" required placeholder=\"Email Address\">\n" +
+    "            </div>\n" +
+    "            <div class=\"input-group\">\n" +
+    "                <span class=\"input-group-addon\"><i class=\"glyphicon glyphicon-lock\"></i></span>\n" +
+    "                <input id=\"password\" type=\"password\" class=\"form-control\" ng-model=\"user.password\" required placeholder=\"Password\">\n" +
+    "            </div>\n" +
+    "            <button type=\"submit\" class=\"btn btn-primary btn-block\">Register</button>\n" +
+    "        </form>\n" +
+    "    </div>\n" +
+    "</div>\n"
+  );
+
+  $templateCache.put("student/create.html",
+    "<div class=\"container\">\n" +
+    "  <form ng-submit=\"addStudent()\">\n" +
+    "    <div class=\"form-group\">\n" +
+    "      <label>Firstname</label>\n" +
+    "      <input type=\"text\" class=\"form-control\" placeholder=\"Student firstname\" ng-model=\"newStudent.firstname\">\n" +
+    "    </div>\n" +
+    "\n" +
+    "    <div class=\"form-group\">\n" +
+    "      <label>Lastname</label>\n" +
+    "      <input type=\"text\" class=\"form-control\" placeholder=\"Student lastname\" ng-model=\"newStudent.lastname\">\n" +
+    "    </div>\n" +
+    "\n" +
+    "    <div class=\"form-group\">\n" +
+    "      <label>Sexe</label>\n" +
+    "      <select ng-model=\"newStudent.sexe\">\n" +
+    "        <option>Male</option>\n" +
+    "        <option>Female</option>\n" +
+    "        <option>Autres</option>\n" +
+    "      </select>\n" +
+    "    </div>\n" +
+    "\n" +
+    "    <div class=\"form-group\">\n" +
+    "      <label>Birthdate</label>\n" +
+    "      <input type=\"date\" class=\"form-control\" placeholder=\"Student birthdate\" ng-model=\"newStudent.birthdate\">\n" +
+    "    </div>\n" +
+    "\n" +
+    "    <div class=\"form-group\">\n" +
+    "      <label>City</label>\n" +
+    "      <input type=\"text\" class=\"form-control\" placeholder=\"Student City\" ng-model=\"newStudent.city\">\n" +
+    "    </div>\n" +
+    "\n" +
+    "    <div class=\"form-group\">\n" +
+    "      <label>Postal Code</label>\n" +
+    "      <input type=\"text\" class=\"form-control\" placeholder=\"Student postal-code\" ng-model=\"newStudent.zipcode\">\n" +
+    "    </div>\n" +
+    "\n" +
+    "    <div class=\"form-group\">\n" +
+    "      <label>Adress</label>\n" +
+    "      <input type=\"text\" class=\"form-control\" placeholder=\"Student adress\" ng-model=\"newStudent.adress\">\n" +
+    "    </div>\n" +
+    "\n" +
+    "    <div class=\"form-group\">\n" +
+    "      <label>Github</label>\n" +
+    "      <input type=\"text\" class=\"form-control\" placeholder=\"Student github\" ng-model=\"newStudent.github\">\n" +
+    "    </div>\n" +
+    "\n" +
+    "    <div class=\"form-group\">\n" +
+    "      <label>Email</label>\n" +
+    "      <input type=\"email\" class=\"form-control\" placeholder=\"Student Email\" ng-model=\"newStudent.email\">\n" +
+    "    </div>\n" +
+    "\n" +
+    "    <button class=\"btn btn-default\" type=\"submit\">Create</button>\n" +
+    "  </form>\n" +
+    "</div>\n"
+  );
+
+  $templateCache.put("user/dashboard.html",
+    "Dashboard de {{user.email}}\n"
+  );
+
+  $templateCache.put("user/navbar.html",
+    "<nav class=\"navbar navbar-default\" role=\"navigation\" ng-controller=\"NavbarController\">\n" +
+    "    <div class=\"container-fluid\">\n" +
+    "        <div class=\"navbar-header\">\n" +
+    "            <button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\"#navbar\">\n" +
+    "        <span class=\"sr-only\">Toggle navigation</span>\n" +
+    "        <span class=\"icon-bar\"></span>\n" +
+    "        <span class=\"icon-bar\"></span>\n" +
+    "        <span class=\"icon-bar\"></span>\n" +
+    "      </button>\n" +
+    "            <a class=\"navbar-brand\" href=\"#\"></a>\n" +
+    "        </div>\n" +
+    "        <div class=\"collapse navbar-collapse\" id=\"navbar\">\n" +
+    "            <ul class=\"nav navbar-nav\">\n" +
+    "                <li ui-sref-active=\"active\"><a ui-sref=\"user.createStudent\" ng-show=\"auth.isAuthenticated()\">Create Student</a></li>\n" +
+    "                <li ui-sref-active=\"active\"><a ui-sref=\"user.dashboard\" ng-show=\"auth.isAuthenticated()\">Dashboard</a></li>\n" +
+    "                <li ui-sref-active=\"active\"><a ui-sref=\"user.profile\" ng-show=\"auth.isAuthenticated()\">Profile</a></li>\n" +
+    "\n" +
+    "            </ul>\n" +
+    "            <ul class=\"nav navbar-nav navbar-right\">\n" +
+    "                <li>\n" +
+    "                <li ui-sref-active=\"active\"><a ui-sref=\"anon.home\">Website</a></li>\n" +
+    "                    <li><a ng-click=\"logout()\" ng-show=\"auth.isAuthenticated()\" href='#'>Logout</a></li>\n" +
+    "                </li>\n" +
+    "            </ul>\n" +
+    "        </div>\n" +
+    "    </div>\n" +
+    "</nav>\n"
+  );
+
+  $templateCache.put("user/profile.html",
+    "Profile de {{user.email}}\n"
+  );
+
+}]);
